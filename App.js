056313e0d1af5fa963b5a35ae5b9796b5640791c
Batuhan Ata Deniz <Atadeniz02@outlook.com>
2025-07-21 18:51:43 +0200

    added bottom navigator and nested navigators

--- App.js
+++ App.js
@@ -5,25 +5,69 @@ import { StyleSheet, Text } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealItemScreen from "./screens/MealItemScreen";
-import FavBtn from "./components/FavBtn";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ProfileScreen from "./screens/ProfileScreen";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+
+const Tab = createBottomTabNavigator();
 
 const Stack = createNativeStackNavigator();
 // Stack is a native object with two properties, where every property holds an object that act as a component
+
+// Separate Stack for nested screens under Meals tab
+function MealsStack() {
+  return (
+    <Stack.Navigator initialRouteName="MealsCategories">
+      <Stack.Screen
+        name="MealsCategories"
+        component={CategoriesScreen}
+        options={{ title: "All Categories" }}
+      />
+      <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+      <Stack.Screen name="MealItemScreen" component={MealItemScreen} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="MealsCategories">
-          <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
-            options={{ title: "All Categories" }}
+        <Tab.Navigator
+          screenOptions={{
+            tabBarActiveTintColor: "green",
+          }}
+        >
+          <Tab.Screen
+            name="MealsTab"
+            component={MealsStack}
+            options={{
+              headerShown: false,
+              title: "Meals",
+              tabBarIcon: ({ size, color }) => (
+                <MaterialCommunityIcons
+                  name="food-apple"
+                  size={size}
+                  color={color}
+                />
+              ),
+            }}
           />
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
-          <Stack.Screen name="MealItemScreen" component={MealItemScreen} />
-        </Stack.Navigator>
+          <Tab.Screen
+            name="Profile"
+            component={ProfileScreen}
+            options={{
+              tabBarIcon: ({ size, color }) => (
+                <MaterialCommunityIcons
+                  name="account"
+                  size={size}
+                  color={color}
+                />
+              ),
+            }}
+          />
+        </Tab.Navigator>
       </NavigationContainer>
     </>
   );
